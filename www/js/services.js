2cb0d3ff9914ab7ec8e3ed126921ea715f82c118
Simon Hogg <sjhogg@gmail.com>
2015-08-04 21:38:59 -0600

    MAPS ANDEVENTS

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,44 @@
 angular.module('starter.services', [])
 
+.factory('Events', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var events = [{
+    id: 0,
+    name: 'Cinema - Terminator 2',
+    attending: '3',
+    image: 'http://img2.wikia.nocookie.net/__cb20120207032138/terminator/images/4/49/T800movie2.jpg'
+  }, {
+    id: 1,
+    name: 'OJs',
+    attending: '15',
+    image: 'https://pbs.twimg.com/profile_images/3410671700/37bbeb67b0b2bff9395a311c478d2512_400x400.png'
+  }, {
+    id: 2,
+    name: 'Falcon CAP',
+    attending: '4',
+    image: 'https://pbs.twimg.com/profile_images/3404292797/188103242f7e2e62cd6289d2ecf6f08b_400x400.jpeg'
+  }];
+
+  return {
+    all: function() {
+      return events;
+    },
+    remove: function(event) {
+      events.splice(events.indexOf(events), 1);
+    },
+    get: function(eventId) {
+      for (var i = 0; i < events.length; i++) {
+        if (events[i].id === parseInt(eventId)) {
+          return events[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
 
